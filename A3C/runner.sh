414fb35bec2aa5327178727a18481ec1875e0441
moxox <mobavarian@gmail.com>
2017-10-13 12:36:43 -0400

    simplified runner.sh slightly

--- A3C/runner.sh
+++ A3C/runner.sh
@@ -1,9 +1,13 @@
 #rm nohup.out
-env=BipedalWalker-v2
+env=LunarLander-v2
 workers=3
-ps=1
-mode=debug-light
-python train.py ps 0 --worker_num $workers  --env $env --ps_num $ps &
-python train.py worker 0 --animate --mode $mode  --env $env --worker_num $workers --ps_num $ps  &
-python train.py worker 1 --worker_num $workers --mode $mode --env $env --ps_num $ps &
-python train.py worker 2 --worker_num $workers --mode $mode --env $env --ps_num $ps &
+ps=2
+for ((i=0; i<$ps; i++)); do
+   python train.py ps $i --worker_num $workers  --env $env --ps_num $ps &
+done
+
+mode=train
+
+for ((i=0; i<$workers; i++)); do
+    python train.py worker $i --mode $mode  --env $env --worker_num $workers --ps_num $ps  &
+done
